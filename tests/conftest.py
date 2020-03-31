c3ad716cce2a5af66f72e6ce06750bdd329e349a
klinga <klingaroo@gmail.com>
2020-03-30 22:50:19 -0400

    basic init test

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,24 +5,6 @@ import requests
 from bookops_worldcat.authorize import AuthorizeAccess
 
 
-# class MockGetTokenResponse:
-#     @staticmethod
-#     def json():
-#         return {
-#             "access_token": "tk_Yebz4BpEp9dAsghA7KpWx6dYD1OZKWBlHjqW",
-#             "token_type": "bearer",
-#             "expires_in": "1199",
-#             "principalID": "",
-#             "principalIDNS": "",
-#             "scopes": "configPlatform context:128807",
-#             "contextInstitutionId": "128807",
-#             "expires_at": "2013-08-23 18:45:29Z",
-#         }
-
-#     # def text():
-#     #     # returned text here
-
-
 @pytest.fixture()
 def mock_credentials():
     return {
@@ -30,47 +12,25 @@ def mock_credentials():
         "library": "OCLC",
         "key": "WSkey",
         "secret": "WSsecret",
-        "authenticating_institution_id": "123456",
-        "context_institution_id": "00001",
-        "principal_id": "00000000-111a-222b-333c-4d444444444d",
-        "principal_idns": "urn:oclc:platform:00001",
-        "scope": ["scope1", "scope2"],
+        "options": {
+            "authenticating_institution_id": "123456",
+            "context_institution_id": "00001",
+            "principal_id": "00000000-111a-222b-333c-4d444444444d",
+            "principal_idns": "urn:oclc:platform:00001",
+            "scope": ["scope1", "scope2"],
+        },
         "oauth_server": "https://oauth.oclc.test.org",
     }
 
 
-# class TestAuthorizeAccessViaClientCredentials:
-#     @staticmethod
-#     def get_token(mock_credentials):
-#         cred = mock_credentials
-#         return AuthorizeAccess(
-#             oauth_server=cred["oauth_server"],
-#             grant_type="client_credentials",
-#             key=cred["key"],
-#             secret=cred["secret"],
-#             options={
-#                 "scope": cred["scope"],
-#                 "authenticating_institution_id": cred["authenticating_institution_id"],
-#                 "context_institution_id": cred["context_institution_id"],
-#             }
-#         )
-
-# @pytest.fixture
-# def mock_access_via_client_creds(monkeypatch):
-#     def
-
-
-# @pytest.fixture
-# def mock_post_token_response(monkeypatch):
-#     """Requests.post() on OCLC token service"""
-
-#     def mock_post(*args, **kwargs):
-#         return MockGetTokenResponse()
-
-#     monkeypatch.setattr(requests, "post", mock_post)
-
-
-# @pytest.fixture(autouse=True)
-# def no_requests(monkeypatch):
-#     """Removes requests.session.Session.request from all test."""
-#     monkeypatch.delattr("requests.session.Session.request")
+@pytest.fixture()
+def mock_access_initiation_via_credentials(mock_credentials):
+    cred = mock_credentials
+    access = AuthorizeAccess(
+        oauth_server=cred["oauth_server"],
+        grant_type="client_credentials",
+        key=cred["key"],
+        secret=cred["secret"],
+        options=cred["options"],
+    )
+    return access
--- tests/test_authorize.py
+++ tests/test_authorize.py
@@ -1,5 +1,4 @@
 import requests
-import requests_mock
 
 from bookops_worldcat import __version__
 from bookops_worldcat.authorize import AuthorizeAccess
@@ -11,39 +10,54 @@ def test_mocked_credentials(mock_credentials):
         "library": "OCLC",
         "key": "WSkey",
         "secret": "WSsecret",
-        "authenticating_institution_id": "123456",
-        "context_institution_id": "00001",
-        "principal_id": "00000000-111a-222b-333c-4d444444444d",
-        "principal_idns": "urn:oclc:platform:00001",
-        "scope": ["scope1", "scope2"],
+        "options": {
+            "authenticating_institution_id": "123456",
+            "context_institution_id": "00001",
+            "principal_id": "00000000-111a-222b-333c-4d444444444d",
+            "principal_idns": "urn:oclc:platform:00001",
+            "scope": ["scope1", "scope2"],
+        },
         "oauth_server": "https://oauth.oclc.test.org",
     }
 
-    # class TestAuthorizeAccess:
+
+class TestAuthorizeAccess:
     """Test AuthorizeAccess and obraining token"""
 
+    def test_access_initiation_via_credentials(
+        self, mock_access_initiation_via_credentials, mock_credentials
+    ):
+        creds = mock_credentials
+        access = mock_access_initiation_via_credentials
 
-def test_token_response(mock_credentials, requests_mock):
-    cred = mock_credentials
-    requests_mock.post(
-        "https://oauth.oclc.test.org/token",
-        headers={"user-agent": f"bookops-worldcat/{__version__}"},
-        auth=(cred["key"], cred["secret"]),
-        data={"grant_type": "client_credentials", "scope": cred["scope"]},
-        timeout=(5, 5),
-        text="OK",
-    )
+        assert access.oauth_server == creds["oauth_server"]
+        assert access.key == creds["key"]
+        assert access.secret == creds["secret"]
+        assert access.grant_type == "client_credentials"
+        assert access.options == creds["options"]
 
-    access = AuthorizeAccess(
-        oauth_server=cred["oauth_server"],
-        grant_type="client_credentials",
-        key=cred["key"],
-        secret=cred["secret"],
-        options={
-            "scope": cred["scope"],
-            "authenticating_institution_id": cred["authenticating_institution_id"],
-            "context_institution_id": cred["context_institution_id"],
-        },
-    )
 
-    # assert access.get_token.json() == mock_post_token_response
+# def test_token_response(mock_credentials, requests_mock):
+#     cred = mock_credentials
+#     requests_mock.post(
+#         "https://oauth.oclc.test.org/token",
+#         headers={"user-agent": f"bookops-worldcat/{__version__}"},
+#         auth=(cred["key"], cred["secret"]),
+#         data={"grant_type": "client_credentials", "scope": cred["scope"]},
+#         timeout=(5, 5),
+#         text="OK",
+#     )
+
+#     access = AuthorizeAccess(
+#         oauth_server=cred["oauth_server"],
+#         grant_type="client_credentials",
+#         key=cred["key"],
+#         secret=cred["secret"],
+#         options={
+#             "scope": cred["scope"],
+#             "authenticating_institution_id": cred["authenticating_institution_id"],
+#             "context_institution_id": cred["context_institution_id"],
+#         },
+#     )
+
+# assert access.get_token.json() == mock_post_token_response
