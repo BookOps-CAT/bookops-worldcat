b90a20e3bb879441cf46e333953baa40962b738a
klinga <klingaroo@gmail.com>
2020-04-07 00:13:42 -0400

    sessions initiations
    - WorlcatSession
    - SearchSession
    - MetaSession

--- bookops_worldcat/session.py
+++ bookops_worldcat/session.py
@@ -8,28 +8,45 @@ from . import __title__, __version__
 class WorldcatSession(requests.Session):
     """Inherits all requests.Session methods"""
 
-    def __init__(self, token=None):
+    def __init__(self, credentials=None):
         requests.Session.__init__(self)
 
-        if not token:
-            raise AttributeError("WorldcatSession requires WorldcatAccessToken object.")
-
-        if type(token).__name__ != "WorldcatAccessToken":
-            raise TypeError("Invalid token object passed in the argument.")
-
-        if token.token_str is None or token.key is None:
-            raise TypeError("Invalid token object.")
+        if not credentials:
+            raise AttributeError(
+                "WorldcatSession requires WorldcatAccessToken object or WSkey."
+            )
 
         self.timeout = (10, 10)
         self.headers.update({"User-Agent": f"{__title__}/{__version__}"})
 
 
 class SearchSession(WorldcatSession):
-    """ Worlcat Search API session. Inherits requests.Session methods"""
+    """ Worlcat Search API wrapper session. Inherits requests.Session methods"""
 
-    def __init__(self, token=None):
-        WorldcatSession.__init__(self, token)
+    def __init__(self, credentials=None):
+        WorldcatSession.__init__(self, credentials)
+
+        if type(credentials) is not str:
+            raise TypeError("OCLC key (WSkey) can be only a string.")
+
+        self.key = credentials
         self.base_url = "http://www.worldcat.org/webservices/catalog/"
-        self.key = token.key
         self.headers.update({"Accept": "application/xml"})
         self.payload = {"wskey": self.key}
+
+
+class MetadataSession(WorldcatSession):
+    """OCLC Metadata API wrapper session. Inherits requests.Session methods"""
+
+    def __init__(self, credentials=None):
+        WorldcatSession.__init__(self, credentials)
+
+        if type(credentials).__name__ != "WorldcatAccessToken":
+            raise TypeError("Invalid token object passed in the argument.")
+
+        if credentials.token_str is None:
+            raise TypeError(
+                "Missing token_str in WorldcatAccessToken object passes as credentials."
+            )
+
+        self.base_url = "https://worldcat.org/"
--- tests/test_session.py
+++ tests/test_session.py
@@ -2,7 +2,7 @@ import pytest
 
 
 from bookops_worldcat import __title__, __version__
-from bookops_worldcat.session import WorldcatSession, SearchSession
+from bookops_worldcat.session import WorldcatSession, SearchSession, MetadataSession
 
 
 class InvalidToken:
@@ -18,34 +18,17 @@ class TestWorldcatSession:
         with pytest.raises(AttributeError):
             assert WorldcatSession()
 
-    def test_invalid_token_object_in_argument(self):
-        token = InvalidToken()
-        with pytest.raises(TypeError):
-            assert WorldcatSession(token=token)
-
-    def test_invalid_token_str_argument(self, mock_token_initiation_via_credentials):
-        token = mock_token_initiation_via_credentials
-        token.token_str = None
-        with pytest.raises(TypeError):
-            assert WorldcatSession(token=token)
-
-    def test_missing_token_key_argument(self, mock_token_initiation_via_credentials):
-        token = mock_token_initiation_via_credentials
-        token.key = None
-        with pytest.raises(TypeError):
-            assert WorldcatSession(token=token)
-
     def test_WorldcatSession_default_timeout(
         self, mock_token_initiation_via_credentials
     ):
         token = mock_token_initiation_via_credentials
-        assert WorldcatSession(token=token).timeout == (10, 10)
+        assert WorldcatSession(credentials=token).timeout == (10, 10)
 
     def test_WorldcatSession_default_headers(
         self, mock_token_initiation_via_credentials
     ):
         token = mock_token_initiation_via_credentials
-        session = WorldcatSession(token=token)
+        session = WorldcatSession(credentials=token)
         assert "User-Agent" in session.headers
         assert session.headers["User-Agent"] == f"{__title__}/{__version__}"
 
@@ -60,29 +43,55 @@ class TestSearchSession:
     def test_invalid_token_type(self):
         token = InvalidToken()
         with pytest.raises(TypeError):
-            assert SearchSession(token=token)
+            assert SearchSession(credentials=token)
+
+    def test_missing_token_key_argument(self, mock_token_initiation_via_credentials):
+        token = mock_token_initiation_via_credentials
+        token.key = None
+        with pytest.raises(TypeError):
+            assert SearchSession(credentials=token)
 
     def test_key_passed_from_token_credentials(
         self, mock_token_initiation_via_credentials
     ):
         token = mock_token_initiation_via_credentials
-        session = SearchSession(token=token)
-        assert session.key == "WSkey"
+        with pytest.raises(TypeError):
+            assert SearchSession(credentials=token)
 
-    def test_oclc_search_api_base_url(self, mock_token_initiation_via_credentials):
-        token = mock_token_initiation_via_credentials
-        session = SearchSession(token=token)
+    def test_oclc_search_api_base_url(self, mock_credentials):
+        key = mock_credentials["key"]
+        session = SearchSession(credentials=key)
         assert session.base_url == "http://www.worldcat.org/webservices/catalog/"
 
-    def test_session_headers(self, mock_token_initiation_via_credentials):
-        token = mock_token_initiation_via_credentials
-        session = SearchSession(token=token)
+    def test_session_headers(self, mock_credentials):
+        key = mock_credentials["key"]
+        session = SearchSession(credentials=key)
         assert "User-Agent" in session.headers
         assert session.headers["User-Agent"] == f"{__title__}/{__version__}"
         assert "Accept" in session.headers
         assert session.headers["Accept"] == "application/xml"
 
-    def test_session_payload(self, mock_token_initiation_via_credentials):
+    def test_session_payload(self, mock_credentials):
+        key = mock_credentials["key"]
+        session = SearchSession(credentials=key)
+        assert session.payload == {"wskey": key}
+
+
+class TestMetadataSession:
+    """Worldcat MetadataSession tests"""
+
+    def test_invalid_token_object_in_argument(self):
+        token = InvalidToken()
+        with pytest.raises(TypeError):
+            assert MetadataSession(credentials=token)
+
+    def test_invalid_token_str_argument(self, mock_token_initiation_via_credentials):
         token = mock_token_initiation_via_credentials
-        session = SearchSession(token=token)
-        assert session.payload == {"wskey": token.key}
+        token.token_str = None
+        with pytest.raises(TypeError):
+            assert MetadataSession(credentials=token)
+
+    def test_base_url(self, mock_token_initiation_via_credentials):
+        token = mock_token_initiation_via_credentials
+        session = MetadataSession(credentials=token)
+        assert session.base_url == "https://worldcat.org/"
