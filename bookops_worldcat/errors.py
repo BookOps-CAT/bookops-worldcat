11eb1ad9ff62f4ce9eeaca104a6b548588de5be4
klinga <klingaroo@gmail.com>
2020-08-21 12:52:06 -0400

    init

---
+++ bookops_worldcat/errors.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+"""
+bookops_worldcat exceptions
+~~~~~~~~~~~~~~~~~~~
+This module contains the set of bookops_worldcat exceptions.
+"""
+
+
+class BookopsWorldcatError(Exception):
+    """Base class for exceptions in this module."""
+
+    pass
+
+
+class TokenRequestError(BookopsWorldcatError):
+    """
+    Exception raised when WorldCat access token is not obtained
+    """
+
+    pass
