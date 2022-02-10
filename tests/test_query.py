52e2feeccbab57b843caae401e72788391bf96fd
klinga <klingaroo@gmail.com>
2022-02-10 09:58:22 -0500

    adds tests for various http codes and exceptions

--- tests/test_query.py
+++ tests/test_query.py
@@ -12,7 +12,7 @@ from bookops_worldcat.metadata_api import MetadataSession, WorldcatAccessToken
 
 
 @pytest.mark.webtest
-def test_live_query(live_keys):
+def test_query_live(live_keys):
     token = WorldcatAccessToken(
         key=os.getenv("WCKey"),
         secret=os.getenv("WCSecret"),
@@ -36,8 +36,44 @@ def test_live_query(live_keys):
         assert query.response.status_code == 200
 
 
+@pytest.mark.http_code(200)
+def test_query_http_200_response(stub_session, mock_session_response):
+    with does_not_raise():
+        req = Request("GET", "https://foo.org")
+        prepped = stub_session.prepare_request(req)
+        query = Query(stub_session, prepped)
+        assert query.response.status_code == 200
+
+
+@pytest.mark.http_code(201)
+def test_query_http_201_response(stub_session, mock_session_response):
+    with does_not_raise():
+        req = Request("GET", "https://foo.org")
+        prepped = stub_session.prepare_request(req)
+        query = Query(stub_session, prepped)
+        assert query.response.status_code == 201
+
+
+@pytest.mark.http_code(206)
+def test_query_http_206_response(stub_session, mock_session_response):
+    with does_not_raise():
+        req = Request("GET", "https://foo.org")
+        prepped = stub_session.prepare_request(req)
+        query = Query(stub_session, prepped)
+        assert query.response.status_code == 206
+
+
+@pytest.mark.http_code(207)
+def test_query_http_207_response(stub_session, mock_session_response):
+    with does_not_raise():
+        req = Request("GET", "https://foo.org")
+        prepped = stub_session.prepare_request(req)
+        query = Query(stub_session, prepped)
+        assert query.response.status_code == 207
+
+
 @pytest.mark.http_code(404)
-def test_http_error_query(stub_session, mock_session_response):
+def test_query_http_404_response(stub_session, mock_session_response):
     header = {"Accept": "application/json"}
     url = (
         "https://americas.metadata.api.oclc.org/worldcat/search/v1/brief-bibs/41266045"
@@ -46,11 +82,45 @@ def test_http_error_query(stub_session, mock_session_response):
     prepped = stub_session.prepare_request(req)
 
     with pytest.raises(WorldcatRequestError) as exc:
-        query = Query(stub_session, prepped)
+        Query(stub_session, prepped)
 
     assert "404 Client Error: 'foo' for url: https://foo.bar?query" in str(exc.value)
 
 
-# @pytest.mark.http_code(200)
-# def test_http_200_code_query(mock_token, mock_session_response):
-#     with
+@pytest.mark.http_code(500)
+def test_query_http_500_response(stub_session, mock_session_response):
+    req = Request("GET", "https://foo.org")
+    prepped = stub_session.prepare_request(req)
+    with pytest.raises(WorldcatRequestError) as exc:
+        Query(stub_session, prepped)
+
+    assert "500 Server Error: 'foo' for url: https://foo.bar?query" in str(exc.value)
+
+
+def test_query_timeout_exception(stub_session, mock_timeout):
+    req = Request("GET", "https://foo.org")
+    prepped = stub_session.prepare_request(req)
+    with pytest.raises(WorldcatRequestError) as exc:
+        Query(stub_session, prepped)
+
+    assert "Connection Error: <class 'requests.exceptions.Timeout'>" in str(exc.value)
+
+
+def test_query_connection_exception(stub_session, mock_connection_error):
+    req = Request("GET", "https://foo.org")
+    prepped = stub_session.prepare_request(req)
+    with pytest.raises(WorldcatRequestError) as exc:
+        Query(stub_session, prepped)
+
+    assert "Connection Error: <class 'requests.exceptions.ConnectionError'>" in str(
+        exc.value
+    )
+
+
+def test_query_unexpected_exception(stub_session, mock_unexpected_error):
+    req = Request("GET", "https://foo.org")
+    prepped = stub_session.prepare_request(req)
+    with pytest.raises(WorldcatRequestError) as exc:
+        Query(stub_session, prepped)
+
+    assert "Unexpected request error: <class 'Exception'>" in str(exc.value)
