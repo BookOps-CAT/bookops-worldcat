4255c9a66ab009420bbb7fe283b2fe5696afd689
klinga <klingaroo@gmail.com>
2022-02-23 22:17:11 -0500

    adds authorization/token refreshing

--- bookops_worldcat/_session.py
+++ bookops_worldcat/_session.py
@@ -10,7 +10,8 @@ from typing import Optional, Tuple, Union
 import requests
 
 from . import __title__, __version__  # type: ignore
-from bookops_worldcat.errors import WorldcatSessionError
+from .authorize import WorldcatAccessToken
+from .errors import WorldcatSessionError, WorldcatAuthorizationError
 
 
 class WorldcatSession(requests.Session):
@@ -18,12 +19,28 @@ class WorldcatSession(requests.Session):
 
     def __init__(
         self,
+        authorization: WorldcatAccessToken,
         agent: Optional[str] = None,
         timeout: Optional[
             Union[int, float, Tuple[int, int], Tuple[float, float]]
         ] = None,
     ) -> None:
-        requests.Session.__init__(self)
+        """
+        Args:
+            authorization:          WorldcatAccessToken instance
+            agent:                  "User-agent" parameter to attached to each
+                                    request in the session
+            timeout:                how long to wait for server to send data
+                                    before giving up
+        """
+        super().__init__()
+
+        self.authorization = authorization
+
+        if not isinstance(self.authorization, WorldcatAccessToken):
+            raise WorldcatSessionError(
+                "Argument 'authorization' must be 'WorldcatAccessToken' object."
+            )
 
         if agent is None:
             self.headers.update({"User-Agent": f"{__title__}/{__version__}"})
@@ -36,3 +53,19 @@ class WorldcatSession(requests.Session):
             self.timeout = (5, 5)
         else:
             self.timeout = timeout  # type: ignore
+
+        self._update_authorization()
+
+    def _get_new_access_token(self) -> None:
+        """
+        Allows to continue sending request with new access token after
+        the previous one expired
+        """
+        try:
+            self.authorization._request_token()
+            self._update_authorization()
+        except WorldcatAuthorizationError as exc:
+            raise WorldcatSessionError(exc)
+
+    def _update_authorization(self) -> None:
+        self.headers.update({"Authorization": f"Bearer {self.authorization.token_str}"})
