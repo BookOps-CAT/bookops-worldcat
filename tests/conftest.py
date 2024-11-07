7403136ea9faf3cbf08a1f5463ca6a50288874d2
Charlotte Kostelic <charlottekostelic@gmail.com>
2024-11-07 11:08:20 -0500

    Bug fixes (#113)
    * added ignore flake8 F401 for test_bookops_worldcat
    
    * added types-pyyaml as dev dependency
    
    * added type annotations removed unnecessary imports
    
    * deleted test.mrc as it is now redundant
    
    * fixed typo in import statement
    
    * exported updated dev-requirements.txt
    
    * added 3.13 to unit-tests.yaml
    
    * fixed live_keys fixture for GITHUB_ACTIONS

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,18 +3,16 @@
 import datetime
 import json
 import os
-
+from typing import Dict, Generator, Union
 import pytest
 import requests
-
-from requests import Response
 
 from bookops_worldcat import WorldcatAccessToken, MetadataSession
 
 
 @pytest.fixture
 def live_keys():
-    if os.name == "nt":
+    if os.name == "nt" and not os.getenv("GITHUB_ACTIONS"):
         fh = os.path.join(os.environ["USERPROFILE"], ".oclc/nyp_wc_test.json")
         with open(fh, "r") as file:
             data = json.load(file)
@@ -24,25 +22,20 @@ def live_keys():
 
 
 @pytest.fixture
-def stub_marc_xml():
+def stub_marc_xml() -> str:
     stub_marc_xml = "<record><leader>00000nam a2200000 a 4500</leader><controlfield tag='008'>120827s2012    nyua          000 0 eng d</controlfield><datafield tag='010' ind1=' ' ind2=' '><subfield code='a'>   63011276 </subfield></datafield><datafield tag='035' ind1=' ' ind2=' '><subfield code='a'>ocn850940548</subfield></datafield><datafield tag='040' ind1=' ' ind2=' '><subfield code='a'>OCWMS</subfield><subfield code='b'>eng</subfield><subfield code='c'>OCWMS</subfield></datafield><datafield tag='100' ind1='0' ind2=' '><subfield code='a'>OCLC Developer Network</subfield></datafield><datafield tag='245' ind1='1' ind2='0'><subfield code='a'>Test Record</subfield></datafield><datafield tag='500' ind1=' ' ind2=' '><subfield code='a'>FOR OCLC DEVELOPER NETWORK DOCUMENTATION</subfield></datafield></record>"
     return stub_marc_xml
 
 
 @pytest.fixture
-def stub_holding_xml():
+def stub_holding_xml() -> str:
     stub_holding_xml = "<record><leader>00000nx  a2200000zi 4500</leader><controlfield tag='004'>312010</controlfield><controlfield tag='007'>zu</controlfield><controlfield tag='008'>1103280p    0   4001uueng0210908</controlfield><datafield ind2=' ' ind1=' ' tag='852'><subfield code='a'>OCWMS</subfield><subfield code='b'>EAST</subfield><subfield code='c'>EAST-STACKS</subfield></datafield><datafield ind2=' ' ind1=' ' tag='876'><subfield code='p'>879456</subfield></datafield></record>"
     return stub_holding_xml
 
 
 @pytest.fixture
-def stub_marc21():
-    fh = os.path.join(
-        os.environ["USERPROFILE"], "github/bookops-worldcat/tests/test.mrc"
-    )
-    with open(fh, "rb") as stub:
-        stub_marc21 = stub.read()
-    return stub_marc21
+def stub_marc21() -> bytes:
+    return b"00266nam a2200097 a 4500008004100000010001700041040002200058100002700080245001600107500004500123\x1e120827s2012    nyua          000 0 eng d\x1e  \x1fa   63011276 \x1e  \x1faOCWMS\x1fbeng\x1fcOCWMS\x1e0 \x1faOCLC Developer Network\x1e10\x1faTest Record\x1e  \x1faFOR OCLC DEVELOPER NETWORK DOCUMENTATION\x1e\x1d"
 
 
 class FakeUtcNow(datetime.datetime):
@@ -59,10 +52,10 @@ def mock_now(monkeypatch):
 class MockAuthServerResponseSuccess:
     """Simulates auth server response to successful token request"""
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.status_code = 200
 
-    def json(self):
+    def json(self) -> Dict[str, str]:
         expires_at = datetime.datetime.strftime(
             datetime.datetime.now() + datetime.timedelta(0, 1199),
             "%Y-%m-%d %H:%M:%SZ",
@@ -84,11 +77,11 @@ class MockAuthServerResponseSuccess:
 class MockAuthServerResponseFailure:
     """Simulates auth server response to successful token request"""
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.status_code = 403
         self.content = b""
 
-    def json(self):
+    def json(self) -> Dict[str, Union[str, int]]:
         return {
             "code": 403,
             "message": "Invalid scope(s): invalid (invalid) [Invalid service specified, Not on key]",
@@ -98,38 +91,38 @@ class MockAuthServerResponseFailure:
 class MockServiceErrorResponse:
     """Simulates web service error responses"""
 
-    def __init__(self, code, json_response, url):
+    def __init__(self, code, json_response, url) -> None:
         self.status_code = code
         self.msg = json_response
         self.url = url
         self.text = f"{json_response}"
 
-    def json(self):
+    def json(self) -> Dict[str, str]:
         return self.msg
 
 
 class MockUnexpectedException:
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
         raise Exception
 
 
 class MockTimeout:
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
         raise requests.exceptions.Timeout
 
 
 class MockConnectionError:
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
         raise requests.exceptions.ConnectionError
 
 
 class MockRetryError:
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
         raise requests.exceptions.RetryError
 
 
-class MockHTTPSessionResponse(Response):
-    def __init__(self, http_code):
+class MockHTTPSessionResponse(requests.Response):
+    def __init__(self, http_code) -> None:
         self.status_code = http_code
         self.reason = "'foo'"
         self.url = "https://foo.bar?query"
@@ -156,7 +149,7 @@ def mock_session_response(request, monkeypatch):
 
 
 @pytest.fixture
-def mock_credentials():
+def mock_credentials() -> Dict[str, str]:
     return {
         "key": "my_WSkey",
         "secret": "my_WSsecret",
@@ -165,7 +158,9 @@ def mock_credentials():
 
 
 @pytest.fixture
-def mock_oauth_server_response(mock_now, *args, **kwargs):
+def mock_oauth_server_response(
+    mock_now, *args, **kwargs
+) -> MockAuthServerResponseSuccess:
     return MockAuthServerResponseSuccess()
 
 
@@ -214,18 +209,20 @@ def mock_retry_error(monkeypatch):
 
 
 @pytest.fixture
-def mock_token(mock_credentials, mock_successful_post_token_response):
+def mock_token(
+    mock_credentials, mock_successful_post_token_response
+) -> WorldcatAccessToken:
     return WorldcatAccessToken(**mock_credentials)
 
 
 @pytest.fixture
-def stub_session(mock_token):
+def stub_session(mock_token) -> Generator[MetadataSession, None, None]:
     with MetadataSession(authorization=mock_token) as session:
         yield session
 
 
 @pytest.fixture
-def stub_retry_session(mock_token):
+def stub_retry_session(mock_token) -> Generator[MetadataSession, None, None]:
     with MetadataSession(
         authorization=mock_token,
         totalRetries=3,
@@ -234,34 +231,3 @@ def stub_retry_session(mock_token):
         allowedMethods=["GET", "POST", "PUT"],
     ) as session:
         yield session
-
-
-@pytest.fixture
-def mock_400_response(monkeypatch):
-    def mock_api_response(*args, **kwargs):
-        msg = {
-            "type": "MISSING_QUERY_PARAMETER",
-            "title": "Validation Failure",
-            "detail": "details here",
-        }
-        url = "https://test.org/some_endpoint"
-        return MockServiceErrorResponse(code=400, json_response=msg, url=url)
-
-    monkeypatch.setattr(requests.Session, "get", mock_api_response)
-    monkeypatch.setattr(requests.Session, "post", mock_api_response)
-    monkeypatch.setattr(requests.Session, "delete", mock_api_response)
-
-
-@pytest.fixture
-def mock_409_response(monkeypatch):
-    def mock_api_response(*args, **kwargs):
-        msg = {
-            "code": {"value": "WS-409", "type": "application"},
-            "message": "Trying to set hold while holding already exists",
-            "detail": None,
-        }
-        url = "https://test.org/some_endpoint"
-        return MockServiceErrorResponse(code=409, json_response=msg, url=url)
-
-    monkeypatch.setattr(requests.Session, "post", mock_api_response)
-    monkeypatch.setattr(requests.Session, "delete", mock_api_response)
